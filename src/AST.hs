7ee48a19b1acaf8601fcd4ae8f6d3d8fd59e9637
Gustavo Castellanos <gustavoaca1997@gmail.com>
2019-10-06 22:27:07 -0400

    Start implementing `printNode`

--- src/AST.hs
+++ src/AST.hs
@@ -1,22 +1,56 @@
 module AST where
 import Tokens
+import Data.Maybe
+
+type Indent = Int
+
+class ASTNode a where
+    printNode :: Indent -> a -> IO ()
+
+putTabs :: Indent -> IO ()
+putTabs indent = putStr $ replicate (2*indent) '-'
 
 -- Start Symbol
 newtype Program =
     Start { external_declarations :: [ExternalDeclaration] }
     deriving (Eq, Show)
 
+instance ASTNode Program where
+    printNode tabs (Start decs) = do
+        putTabs tabs
+        putStrLn "Program:"
+        foldl1 (>>) $ map (printNode (tabs+1)) decs
+        putStr "\n"
+
+-- Global var declarations and function declarations
 data ExternalDeclaration =
     ExternalFunctionDeclaration FunctionDeclaration |
     ExternalVarDeclaration VarDeclaration
     deriving (Eq, Show)
 
+instance ASTNode ExternalDeclaration where
+    printNode tabs (ExternalFunctionDeclaration funcDec) =
+        printNode tabs funcDec
+
+    printNode tabs (ExternalVarDeclaration varDec) =
+        printNode tabs varDec
+
 -- Function Declaration
 data FunctionDeclaration =
     FunctionDec {   func_id :: Id, func_type :: Type, 
-                    func_params :: [(Id, Type)], 
+                    func_params :: [VarDeclaration], 
                     func_body :: Block  }
     deriving (Eq, Show)
+
+instance ASTNode FunctionDeclaration where
+    printNode tabs funcDec = do
+        putTabs tabs
+        putStr "Function Declaration: "
+        putStr $ show (func_id funcDec) ++ "("
+        foldl1 (>>) $ map (printNode (tabs+1)) $ func_params funcDec
+        putStrLn $ ": " ++ show $ token $ type_token $ funct_type funcDec
+        printNode (tabs+1) $ func_body funcDec
+        putStr "\n"
 
 -- Variable declaration
 data VarDeclaration =
@@ -24,15 +58,33 @@ data VarDeclaration =
                 var_init :: Maybe Expression    }
     deriving (Eq, Show)
 
+instance ASTNode VarDeclaration where
+    printNode tabs varDec = do
+        printNode (tabs+1) $ var_id varDec
+        putStr ": "
+        printNode (tabs+1) $ var_type varDec
+        maybe (putStr "") (printNode (tabs+1)) $ var_init varDec
+        putStr "\n"
+
 -- Identifier
 newtype Id = 
     Id    { id_token :: Token }
     deriving (Eq, Show)
 
+instance ASTNode Id where
+    printNode tabs identifier = do
+        putTabs tabs
+        putStrLn $ "Identifier: " ++ show $ token $ id_token identifier
+
 -- Type
 newtype Type = 
     Type { type_token :: Token } 
     deriving (Eq, Show)
+
+instance ASTNode Type where
+    printNode tabs dataType = do
+        putTabs tabs
+        putStrLn $ "Data Type: " ++ show $ token $ id_token identifier
 
 -- Expression
 data Expression =
