efa3127f2697fcf3f4551753e75b236c0a1d298e
David <dave00dark@gmail.com>
2019-10-20 19:55:53 -0400

    fixes #7

--- test/lexer/FunctionSpec.hs
+++ test/lexer/FunctionSpec.hs
@@ -1,41 +1,37 @@
 module FunctionSpec where
 
-    import Test.Hspec
-    import Lexer
-    import Utils
-    import Tokens
+import Test.Hspec
+import Lexer
+import Utils
+import Tokens
+
+spec :: Spec
+spec = describe "Lexer.Functions" $ do
+
+    it "accepts `track f(n: quarter): half` as a valid Function Token List" $ do
+        let x = "track f(n: quarter): half"
+        let atok = getTok $ runAlexScan x    
+        case atok of
+            [_, IdToken idf _ _, _, IdToken idn _ _, _, QuarterToken tkq _ _, _, _, HalfToken tkh _ _ ] -> do
+                idf `shouldBe` "f"
+                idn `shouldBe` "n"
+                tkq `shouldBe` "quarter"
+                tkh `shouldBe` "half"
+            _ -> error "Pattern failed"
     
-    spec :: Spec
-    spec = describe "Lexer" $ do
-    
-        it "accepts `0` as a valid int literal" $ do
-            let x = "track f(n: quarter): half"
-            let atok = getTok $ runAlexScan x    
-            case atok of
-                [_, IdToken idf _ _, _, IdToken idn _ _, _, QuarterToken tkq _ _, _, _, HalfToken tkh _ _ ] -> do
-                    idf `shouldBe` "f"
-                    idn `shouldBe` "n"
-                    tkq `shouldBe` "quarter"
-                    tkh `shouldBe` "half"
-                _ -> error "Pattern failed"
-    
-        -- it "accepts `1` as a valid int literal" $ do
-        --     let x = "1"
-        --     s <- runAlexScan x
-        --     case s of
-        --         Just toks -> do
-        --             let atok = getToken $ head toks
-        --             atok `shouldBe` IntToken
-        --         Nothing ->
-        --             error "rejected as an invalid token"
-    
-        -- it "accepts `100` as a valid int literal" $ do
-        --     let x = "100"
-        --     s <- runAlexScan x
-        --     case s of
-        --         Just toks -> do
-        --             let atok = getToken $ head toks
-        --             atok `shouldBe` IntToken
-        --         Nothing ->
-        --             error "rejected as an invalid token"
-    
+    it "Accepts `track f():whole` as a valid Function Token List" $ do
+        let x = "track f():whole"
+        let atok = getTok $ runAlexScan x
+        case atok of
+            [_, IdToken idf _ _, _, _, _, WholeToken tkh _ _ ] -> do
+                idf `shouldBe` "f"
+                tkh `shouldBe` "whole"
+            _ -> error "Pattern failed"
+
+    it "Accepts `track f()` as a valid Function Token List" $ do
+        let x = "track f()"
+        let atok = getTok $ runAlexScan x
+        case atok of
+            [_, IdToken idf _ _, _, _] -> do
+                idf `shouldBe` "f"
+            _ -> error "Pattern failed"
