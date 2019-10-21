05937a933388f8c11591862742b860b82b700e0d
Gustavo Castellanos <gustavoaca1997@gmail.com>
2019-10-20 20:12:45 -0400

    se crea el archivo Semantic.Data.hs con un layout ahí

---
+++ src/Semantic/Data.hs
@@ -0,0 +1,9 @@
+module Semantic.Data where
+
+-- Category con info adicional
+
+-- Entry, con scope, categoria, token, tipo, cualquier otra cosa chupalo
+
+-- Alias SymbolTable para un mapa de String -> [Entry]
+
+-- Alias Scopes para un set de int
