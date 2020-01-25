a88ad26359020bc5a8265173979b9bd4ad342f5f
Gustavo Castellanos <gustavoaca1997@gmail.com>
2020-01-25 16:07:09 -0400

    Update TACType

--- TAC/TACType.hs
+++ TAC/TACType.hs
@@ -12,24 +12,32 @@ data (SymEntryCompatible a) => ThreeAddressCode a b = ThreeAddressCode
   deriving (Eq)
 
 instance (SymEntryCompatible a, Show a, Show b) => Show (ThreeAddressCode a b) where
-  show (ThreeAddressCode Assign (Just x) (Just y) _) = show x ++ " = " ++ show y
-  show (ThreeAddressCode Add (Just x) (Just y) (Just z)) = show x ++ " = " ++ show y ++ " + " ++ show z
-  show (ThreeAddressCode Minus (Just x) (Just y) Nothing) = show x ++ " = -" ++ show y 
-  show (ThreeAddressCode Sub (Just x) (Just y) (Just z)) = show x ++ " = " ++ show y ++ " - " ++ show z
-  show (ThreeAddressCode Mult (Just x) (Just y) (Just z)) = show x ++ " = " ++ show y ++ " * " ++ show z
-  show (ThreeAddressCode Div (Just x) (Just y) (Just z)) = show x ++ " = " ++ show y ++ " / " ++ show z
+  show (ThreeAddressCode Assign (Just x) (Just y) _)          = show x ++ " = " ++ show y
+  show (ThreeAddressCode Add (Just x) (Just y) (Just z))      = show x ++ " = " ++ show y ++ " + " ++ show z
+  show (ThreeAddressCode Minus (Just x) (Just y) Nothing)     = show x ++ " = -" ++ show y 
+  show (ThreeAddressCode Sub (Just x) (Just y) (Just z))      = show x ++ " = " ++ show y ++ " - " ++ show z
+  show (ThreeAddressCode Mult (Just x) (Just y) (Just z))     = show x ++ " = " ++ show y ++ " * " ++ show z
+  show (ThreeAddressCode Div (Just x) (Just y) (Just z))      = show x ++ " = " ++ show y ++ " / " ++ show z
   show (ThreeAddressCode (Cast _ toType) (Just x) (Just y) _) = show x ++ " = " ++ toType ++ "(" ++ show y ++ ")"
+  show (ThreeAddressCode Not (Just x) (Just y) _)             = show x ++ " = ~" ++ show y
+  show (ThreeAddressCode And (Just x) (Just y) (Just z))      = show x ++ " = " ++ show y ++ " && " ++ show z
+  show (ThreeAddressCode Or (Just x) (Just y) (Just z))       = show x ++ " = " ++ show y ++ " || " ++ show z
+  show (ThreeAddressCode GoTo Nothing Nothing (Just label))   = "goto " ++ show label
+  show (ThreeAddressCode GoTo Nothing Nothing Nothing)        = "goto __"
+  show (ThreeAddressCode If Nothing (Just b) (Just label))    = "if " ++ show b ++ " then goto " ++ show label
+  show (ThreeAddressCode If Nothing (Just b) Nothing)         = "if " ++ show b ++ " then goto __"
+
   show tac = show (tacLvalue tac) ++ " = " ++ show (tacRvalue1 tac) ++ " (?) " ++ show (tacRvalue2 tac)
 
 data (SymEntryCompatible a) => Operand a b = 
   Variable a | 
-  Constant b | 
+  Constant (String, b) | 
   Label Int
   deriving (Eq)
 
 instance (SymEntryCompatible a, Show a, Show b) => Show (Operand a b) where
   show (Variable x) = show x
-  show (Constant c) = show c
+  show (Constant c) = fst c
   show (Label l) = show l
 
 data Operation =
